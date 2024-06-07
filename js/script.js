60e91d1726fdcb3f3609e662aab03dec5dcad0d6
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-06-07 13:52:04 +0300

    add about and lists

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 $('input[name="phone"]').mask("+375(99)999-99-99");
+function openCity(cityName) {
+    let i;
+    let x = document.getElementsByClassName("city");
+    let y = cityName.length
+    document.getElementById('services1').classList.add('button_green1');
+    document.getElementById('services2').classList.add('button_green1');
+    for (i = 0; i < x.length; i++) {
+
+        x[i].style.display = "none";
+        if(y>5){
+            document.getElementById('services1').classList.remove('button_green1');
+        }
+        else {
+            document.getElementById('services2').classList.remove('button_green1');
+        }
+    }
+    document.getElementById(cityName).style.display = "block";
+}
