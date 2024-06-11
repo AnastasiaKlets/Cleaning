050beb4b4dfe7c0b8f57d0d9b3426456b4422846
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-06-11 13:36:36 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -214,7 +214,15 @@ function slider({containerSelector, slideSelector, nextSlideSelector, prevSlideS
         if (containerSelector.includes('gallery')) {
             let gallery_images = document.querySelectorAll('.gallery_slide .pc')
             let new_src = gallery_images[slideIndex - 1].getAttribute('src');
+            let data = gallery_images[slideIndex - 1].getAttribute('data-src');
             let main_image = document.querySelector('.gallery_image img');
+            if (data) {
+                main_image.setAttribute('data-src', data);
+                main_image.setAttribute('data-fancybox', '');
+            } else {
+                main_image.removeAttribute('data-src');
+                main_image.removeAttribute('data-fancybox');
+            }
             main_image.setAttribute('src', new_src);
         }
         if (currentCounter) {
@@ -329,7 +337,6 @@ if (document.querySelector('.gallery_field') != null) {
         elementsPerPage: 3,
         elementsPerPageMobile: 1,
         columnGap: 10,
-        duration: 3000,
         swipe: true,
     });
 }
